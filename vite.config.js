a6c35fda484ff8b5d6cf99f9ced95547e4c2f259
forevermarsh <marshmansur004@gmail.com>
2024-08-16 18:22:34 +0300

    first

---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.js',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    manifest: {
+      name: 'walamin',
+      short_name: 'walamin',
+      description: 'walamin green solutions',
+      theme_color: '#ffffff',
+
+      icons: [{
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      }, {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }, {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      }],
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
